918018d5627e82057a52835c5fed7bcde4b5b463
Rohit <rohitnarnolia88@gmail.com>
2026-04-04 09:21:39 +0530

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+#![cfg_attr(not(test), no_std)]
+
+use quasar_lang::prelude::*;
+
+declare_id!("3AcEyc1RWdVrak9dvWigC3ppoSr2xhBeS8LkbPcgWZzg");
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    pub payer: &'info mut Signer,
+    pub system_program: &'info Program<System>,
+}
+
+impl<'info> Initialize<'info> {
+    #[inline(always)]
+    pub fn initialize(&self) -> Result<(), ProgramError> {
+        Ok(())
+    }
+}
+
+#[program]
+mod escrow_quasar {
+    use super::*;
+
+    #[instruction(discriminator = 0)]
+    pub fn initialize(ctx: Ctx<Initialize>) -> Result<(), ProgramError> {
+        ctx.accounts.initialize()
+    }
+}
+
+#[cfg(test)]
+mod tests;
